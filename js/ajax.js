04fc7d8c7ed29cdb0da5206ef57613fd403e3409
Atahualpa Silva F <atahualpasilva@gmail.com>
2015-07-15 14:14:00 -0300

    Agregue data a través de ajax con un archivo usuario.json

--- js/ajax.js
+++ js/ajax.js
@@ -2,4 +2,11 @@ $(function() {
 	$.get('../logos_footer.html', function(data) {
 		$('footer').append(data);
 	});
+
+	$.getJSON('../data/usuario.json', function(data) {
+		var avatar = new Image();
+		avatar.src = data.avatar;
+		avatar.title = data.nombre + ' '  + data.apellido;
+		$('#avatar').append(avatar);
+	})
 });
